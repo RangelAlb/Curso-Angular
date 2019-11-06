a66e122b25f53218e0b45cb959d5b52a0d1405ff
Rangel Albuquerque <rangel_albuquerque_2012@hotmail.com>
2019-11-06 08:48:37 -0300

    Aula 5 - Componentes

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { MeuPrimeiroComponent } from "./meu-primeiro/meu-primeiro.component";
-import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { MeuPrimeiro2Component } from "./meu-primeiro2/meu-primeiro2.component";
+import { CursosModule } from "./cursos/cursos.module";
 
 @NgModule({
   declarations: [AppComponent, MeuPrimeiroComponent, MeuPrimeiro2Component],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, CursosModule],
   providers: [],
   bootstrap: [AppComponent]
 })
