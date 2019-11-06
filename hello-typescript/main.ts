f21de0f7c88fe3ca12eb7be5133fd58c88b2b027
Rangel Albuquerque <rangel_albuquerque_2012@hotmail.com>
2019-11-06 08:28:42 -0300

    Aula 4 - Typescript

---
+++ hello-typescript/main.ts
@@ -0,0 +1,20 @@
+var minhaVar = "Minha Variável";
+
+function minhaFuncao1(x, y) {
+  return x + y;
+}
+
+let num1 = 2;
+var PI = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map(valor => valor * 2);
+
+class Matematica1 {
+  soma(x, y) {
+    return x + y;
+  }
+}
+
+var n1: any = "teste";
+n1 = 4;
