422caf3e949deeba8ef8d1617c4555f4f83c9a7e
Rangel Albuquerque <rangel_albuquerque_2012@hotmail.com>
2019-11-07 10:35:56 -0300

    Aula 7 - Introdução Template

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-cursos',
-  templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  selector: "app-cursos",
+  templateUrl: "./cursos.component.html",
+  styleUrls: ["./cursos.component.css"]
 })
 export class CursosComponent implements OnInit {
+  nomePortal: string;
 
-  constructor() { }
+  cursos: string[] = ["Java", "Ext JS", "Angular"];
 
-  ngOnInit() {
+  constructor() {
+    this.nomePortal = "http://loiane.training";
+
+    for (let i = 0; i < this.cursos.length; i++) {
+      let curso = this.cursos[i];
+    }
   }
 
+  ngOnInit() {}
 }
