77673ad1c5ab00ea167155e5c965c93d872092ac
Rangel Albuquerque <rangel_albuquerque_2012@hotmail.com>
2019-11-08 09:07:12 -0300

    Aula 7 - Services

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { CursosService } from "./cursos.service";
 
 @Component({
   selector: "app-cursos",
@@ -8,14 +9,18 @@ import { Component, OnInit } from "@angular/core";
 export class CursosComponent implements OnInit {
   nomePortal: string;
 
-  cursos: string[] = ["Java", "Ext JS", "Angular"];
+  cursos: string[];
 
-  constructor() {
+  constructor(private cursosService: CursosService) {
     this.nomePortal = "http://loiane.training";
 
-    for (let i = 0; i < this.cursos.length; i++) {
-      let curso = this.cursos[i];
-    }
+    // for (let i = 0; i < this.cursos.length; i++) {
+    //   let curso = this.cursos[i];
+    // }
+
+    //var servico = new CursosService();
+
+    this.cursos = this.cursosService.getCursos();
   }
 
   ngOnInit() {}
--- primeiro-projeto/src/app/cursos/cursos.module.ts
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CursosComponent } from "./cursos.component";
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoDetalheComponent } from "./curso-detalhe/curso-detalhe.component";
+import { CursosService } from "./cursos.service";
 
 @NgModule({
   declarations: [CursosComponent, CursoDetalheComponent],
   imports: [CommonModule],
-  exports: [CursosComponent]
+  exports: [CursosComponent],
+  providers: [CursosService]
 })
 export class CursosModule {}
