fd7bf487ba835f1954dbdd49a7bf37791b8930e2
Rangel Albuquerque <rangel_albuquerque_2012@hotmail.com>
2019-11-12 09:53:42 -0300

    Aula 10 - Class e Style Binding

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DataBindingComponent } from './data-binding/data-binding.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { DataBindingComponent } from "./data-binding/data-binding.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DataBindingComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, DataBindingComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
